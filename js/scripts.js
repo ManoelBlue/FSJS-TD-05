206d10832d0b08b6fe3676204655a543d02f818e
Manoel Blue <manoelbluee@gmail.com>
2021-01-21 07:54:37 +0100

    add error handling

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,4 @@
 fetch("https://randomuser.me/api/?results=12&inc=name,location,email,picture")
     .then(response => response.json())
     .then(json => console.log(json))
+    .catch(error => console.error(error))
