4b8583b99364cac6dd5fb7f8bcdac667d6d1076d
Manoel Blue <manoelbluee@gmail.com>
2021-01-21 08:18:40 +0100

    display modal window on click

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,50 @@ fetch("https://randomuser.me/api/?results=12&inc=name,location,email,picture")
     .then(json => json.results.map(user => {
         console.log(user);
         gallery.insertAdjacentHTML("beforeend", `
-        <div class="card">
-            <div class="card-img-container">
-                <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+            <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                    <p class="card-text">${user.email}</p>
+                    <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+                </div>
             </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
-                <p class="card-text">${user.email}</p>
-                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
-            </div>
-        </div>
         `)
     }))
-    .catch(error => console.error(error))
+    .catch(error => console.error(error));
+
+// Pop up modal window on a click:
+gallery.addEventListener("click", (e) => {
+    console.dir(e.target);
+    console.dir(e.target.closest(".card"));
+
+    const card = e.target.closest(".card");
+
+    if(card) {
+        gallery.insertAdjacentHTML("afterend", `
+            <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+    
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>
+        `)
+    }
+
+});
