d4c09a02e3fb38263e3e15fee76bd9b1573a8df8
Manoel Blue <manoelbluee@gmail.com>
2021-01-21 08:03:48 +0100

    display the users

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
+// Global variables:
+const gallery = document.getElementById("gallery");
+
 // Fetch users:
 fetch("https://randomuser.me/api/?results=12&inc=name,location,email,picture")
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => json.results.map(user => {
+        console.log(user);
+        gallery.insertAdjacentHTML("beforeend", `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="card-text">${user.email}</p>
+                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+            </div>
+        </div>
+        `)
+    }))
     .catch(error => console.error(error))
